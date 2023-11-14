3c4ad35480a69f155d4d5871ee7c3ed5881e39ce
Gabrie Andres <gabrielrubioduque22@gmail.com>
2023-11-14 11:32:41 -0500

    hasta el ejercicio 9

---
+++ src/main/java/Logica/Gabriel_Rubio_Taller_Ciclos_SENA.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Logica;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Gabriel_Rubio_Taller_Ciclos_SENA {
+
+    public static void main(String[] args) {
+    }
+}
