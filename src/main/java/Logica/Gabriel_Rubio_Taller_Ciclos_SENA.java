83e9f0fa9d53fa29d68ccc91b778dc8b68fb0741
Gabrie Andres <gabrielrubioduque22@gmail.com>
2023-11-17 10:30:23 -0500

    añadidos tablas de multiplicar ejercicio 12

--- src/main/java/Logica/Gabriel_Rubio_Taller_Ciclos_SENA.java
+++ src/main/java/Logica/Gabriel_Rubio_Taller_Ciclos_SENA.java
@@ -8,8 +8,22 @@ package Logica;
  *
  * @author USUARIO
  */
+
 public class Gabriel_Rubio_Taller_Ciclos_SENA {
 
     public static void main(String[] args) {
+        System.out.println("Procesando... ");
+        for(int i = 1; i<=100; i++){
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+               
+            }
+            System.out.print("\rProgreso: " + i + "%");
+            
+        }
+        
+         
     }
 }
